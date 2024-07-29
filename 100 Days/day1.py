b309dce6e6f6739691f2ee481caa784d9930cbc5
Adarsh Madhusoodanan <iadarshmadhusoodanan@gmail.com>
2024-07-29 23:52:47 +0530

    day 1

--- 100 Days/day1.py
+++ 100 Days/day1.py
@@ -1,3 +1,8 @@
 #******** Day 1 of 100 days of coding *******
 
-#
+# Full name generator
+
+print("Welcome to the Full Name Generator.")
+first_name = input("What is your first name?\n")
+second_name = input("What is your last name?\n")
+print("Your Full name is: " + first_name + " " + second_name)
