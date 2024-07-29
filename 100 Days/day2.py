93fca1acce5355f4164df9310b23b1f61cc81e88
Adarsh Madhusoodanan <iadarshmadhusoodanan@gmail.com>
2024-07-30 00:09:52 +0530

    day 2

---
+++ 100 Days/day2.py
@@ -0,0 +1,10 @@
+#******** Day 2 of 100 days of coding *******
+
+# Tip Calculator
+
+print("Welcome to the tip calculator")
+total_bill = int(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+No_people = int(input("How many people to split the bill? "))
+pay_amount = (total_bill + tip ) / No_people
+print(f"Each person should pay: ${pay_amount}")
