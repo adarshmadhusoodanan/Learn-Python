6a5289dd19aa2f6b854a58ee7937540db35a4733
Adarsh Madhusoodanan <iadarshmadhusoodanan@gmail.com>
2024-07-30 00:29:28 +0530

    day2 update

--- 100 Days/day2.py
+++ 100 Days/day2.py
@@ -5,6 +5,6 @@
 print("Welcome to the tip calculator")
 total_bill = int(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
-No_people = int(input("How many people to split the bill? "))
-pay_amount = (total_bill + tip ) / No_people
+no_people = int(input("How many people to split the bill? "))
+pay_amount = (total_bill + tip ) / no_people
 print(f"Each person should pay: ${pay_amount}")
