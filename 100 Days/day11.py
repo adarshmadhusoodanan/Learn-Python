9a13ed0167bddabbc3d53faee9f9fb3587d5ca02
Adarsh Madhusoodanan <iadarshmadhusoodanan@gmail.com>
2024-08-03 03:53:32 +0530

    sum

--- 100 Days/day11.py
+++ 100 Days/day11.py
@@ -10,4 +10,43 @@ logo='''
       |  \/ K|                            _/ |                
       '------'                           |__/                  
 '''
-print(logo)
+import random
+
+def sum(user):
+    sum=0
+    for i in user:
+        sum += i
+    return sum
+
+
+def game():
+    cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+
+    user =[]
+    number1=random.choice(cards)
+    user.append(number1)
+    cards.remove(number1)
+    user.append(random.choice(cards))
+    print(f"Your card: {user}")
+
+    user_sum = sum(user)
+    print(user_sum)
+
+    computer=[]
+    num2 =random.choice(cards)
+    computer.append(num2)
+    print(f"Computer first card:{num2}")
+    cards.remove(num2)
+    computer.append(random.choice(cards))
+
+    computer_sum = sum(computer)
+    print(computer_sum)
+
+
+play = input("Do you want to play a game of Blackjack? Type 'y' or 'n':")
+
+if play =="y":
+    print(logo)
+    game()
+
+
