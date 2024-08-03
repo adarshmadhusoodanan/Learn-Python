f787c9e9d6a74181048f73b9f4a0d73156f3de4a
Adarsh Madhusoodanan <iadarshmadhusoodanan@gmail.com>
2024-08-03 18:53:06 +0530

    day11

--- 100 Days/day11.py
+++ 100 Days/day11.py
@@ -18,6 +18,21 @@ def sum(user):
         sum += i
     return sum
 
+def check(user,computer):
+    if user>21:
+        result=0 #user lost
+    elif computer>21:
+        result = 1 #user win
+    elif user==21 and computer==21:
+        result = 2 #draw
+    elif user>computer and user<=21:
+        result = 3 #user win
+    elif computer>user and computer<=21:
+        result = 4 #user lost
+
+    return result
+    
+
 
 def game():
     cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
@@ -27,10 +42,14 @@ def game():
     user.append(number1)
     cards.remove(number1)
     user.append(random.choice(cards))
-    print(f"Your card: {user}")
+    
 
     user_sum = sum(user)
-    print(user_sum)
+    
+
+            
+    print(f"Your card: {user}, current score:{user_sum}")
+    
 
     computer=[]
     num2 =random.choice(cards)
@@ -40,13 +59,50 @@ def game():
     computer.append(random.choice(cards))
 
     computer_sum = sum(computer)
-    print(computer_sum)
+    #print(computer_sum)
+
+    
+
+    if user_sum<21:
+        again = input("Type 'y' to get another card, type 'n' to pass:")
+        if again =='y':
+            user.append(random.choice(cards))
+            user_sum = sum(user)
+            print(f"Your card: {user}, final score:{user_sum}")
+            print(f"computer card: {computer}, final score:{computer_sum}")
+            result=check(user_sum,computer_sum)
+        else:
+            print(f"Your card: {user}, final score:{user_sum}")
+            print(f"computer card: {computer}, final score:{computer_sum}")
+            result=check(user_sum,computer_sum)
+            return result,user_sum
+    else:
+        result=check(user_sum,computer_sum)      
 
 
-play = input("Do you want to play a game of Blackjack? Type 'y' or 'n':")
+    
+    return result,user_sum
+    
 
-if play =="y":
+def play():
+    result,user_sum=game()
+    if result == 0:
+        print("You Lost")  
+    elif result== 1:
+        print("You Win")
+    elif result == 2:
+        print("Draw")
+    elif result == 3:
+        print("You Win")
+    elif result == 4:
+        print("User Lost")
+
+
+start = input("Do you want to play a game of Blackjack? Type 'y' or 'n':")
+
+if start =="y":
     print(logo)
-    game()
+    play()
+    
 
 
